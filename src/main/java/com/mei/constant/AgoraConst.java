68cc5fe51b201e9e4d2a56e450769a5dcf5731a0
xavier777 <1904006196@qq.com>
2021-03-29 11:32:16 +0800

    README 文件的编写

--- src/main/java/com/mei/constant/AgoraConst.java
+++ src/main/java/com/mei/constant/AgoraConst.java
@@ -8,10 +8,10 @@ public final class AgoraConst {
     /**
      * 项目的 App ID
      */
-    public static final String appId = "e932c462e1f84776930ed75323690c1f";
+    public static final String appId = "";
 
     /**
      * 项目的 App 证书
      */
-    public static final String appCertificate = "0d23a694247e41348f0e67f40d97f512";
+    public static final String appCertificate = "";
 }
